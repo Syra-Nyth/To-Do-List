b3464a1e7a51b681d273857516b5b4decfc1b85b
Syra-Nyth <48935704+Syra-Nyth@users.noreply.github.com>
2020-04-03 09:53:47 +1300

    To-Do List
    have array
    have loop
    added adding a new todo
    have input and button

--- src/index.svelte
+++ src/index.svelte
@@ -1,3 +1,30 @@
-<h1>Svelte</h1>
+<script>
+    let newToDo = ""
+    let todos = [
+        "Write essay",
+        "Code to-do list",
+        "Work on stats",
+        "Complete physics questions"
+    ]
 
-<p>Welcome to coding with Svelte!</p>
+    function addToDo() {
+        // add the newToDo to the end of the todos array
+        todos = [...todos, newToDo]
+    }
+</script>
+
+<h1 class="title">To-Do List</h1>
+
+<label>
+    To-Do: 
+    <input type="text" bind:value={newToDo}>
+</label>
+
+<button on:click={addToDo}>Add To-Do</button>
+
+<ul class="list">
+    {#each todos as todo}
+    <li class="list-item">{todo}</li>
+    {/each}
+
+</ul>
