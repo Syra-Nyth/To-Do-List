0ef79316b0407f24d22cadda33043437d6e776aa
Syra-Nyth <48935704+Syra-Nyth@users.noreply.github.com>
2020-04-03 11:01:29 +1300

    Update index.svelte
    Add an item to an array
    Remove an item from an array
    Send a variable to a function

--- src/index.svelte
+++ src/index.svelte
@@ -1,5 +1,6 @@
 <script>
     let newToDo = ""
+    let place = 0
     let todos = [
         "Write essay",
         "Code to-do list",
@@ -11,20 +12,35 @@
         // add the newToDo to the end of the todos array
         todos = [...todos, newToDo]
     }
+
+    function removeToDo(index) {
+        // remove the todo at index form the todos array
+        todos = [...todos.slice(0, index), ...todos.slice(index + 1)]
+    }
 </script>
 
 <h1 class="title">To-Do List</h1>
 
-<label>
-    To-Do: 
-    <input type="text" bind:value={newToDo}>
-</label>
+<div id="addToDo">
+    <label>
+        To-Do: 
+        <input type="text" bind:value={newToDo}>
+    </label>
 
-<button on:click={addToDo}>Add To-Do</button>
+    <button on:click={addToDo}>Add To-Do</button>
+</div>
+
+<div id="removeToDo">
+    <label>
+        Remove: 
+        <input type="number" bind:value={place}>
+    </label>
+
+    <button on:click={ () => { removeToDo(place) } }>Remove: </button>
+</div>
 
 <ul class="list">
-    {#each todos as todo}
-    <li class="list-item">{todo}</li>
+    {#each todos as todo, index}
+    <li class="list-item">{index}: {todo}</li>
     {/each}
-
 </ul>
